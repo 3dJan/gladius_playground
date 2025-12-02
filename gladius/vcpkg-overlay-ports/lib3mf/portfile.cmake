a3da112d15ccc9404fb9406b6815e258d8d094ce
Jan Orend <56254096+3dJan@users.noreply.github.com>
2025-12-02 19:39:25 +0100

    [portfile.cmake] Add patch to fix missing algorithm in NMR_ResourceDependencySorter

--- gladius/vcpkg-overlay-ports/lib3mf/portfile.cmake
+++ gladius/vcpkg-overlay-ports/lib3mf/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_github(
     REPO 3MFConsortium/lib3mf
     REF "3djan/volumetric_beamlattice_gradient"
     SHA512 912107f0f4221d95424765acc329ffddffca2bb284ea1dbb10746c4022ae496e1168a21abe243edb37cbbe9857817246033162d7bf49422fec91789aee0dd46e
+    PATCHES
+        fix-missing-algorithm.patch
 )
 
 # Normalize target_link_libraries signature by inserting PRIVATE for plain-signature lines
